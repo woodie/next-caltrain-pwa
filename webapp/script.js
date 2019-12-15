257ef670eebce0fb7b71999b10dcae4abac8d675
John Woodell <woodie@netpress.com>
2019-12-14 16:56:19 -0800

    Work on getting schedule to load.

--- webapp/script.js
+++ webapp/script.js
@@ -1,4 +1,5 @@
 let prefs = new Preferences(caltrainServiceData.southStops);
+let sched = new CaltrainService();
 
 var appState = {
   fullScreen: false,
@@ -17,71 +18,63 @@ var fullScreenView = function () {
 
 var loadSchedule = function () {
   // Set the time
-  var d = new Date();
-  var hr = d.getHours();
-  var min = d.getMinutes();
+  let timeNow = new Date();
+  let hr = timeNow.getHours();
+  let min = timeNow.getMinutes();
   if (min < 10) { min = "0" + min; }
-  var ampm = "am";
+  let ampm = "am";
   if( hr > 12 ) { hr -= 12; ampm = "pm"; } // hr + ":" + min + ampm ;
-  //document.getElementById('timeNow').innerHTML = `${hr}:${min}${ampm}`;
-  fakeTime = '7:58am';
-  document.getElementById('timeNow').innerHTML = fakeTime;
-  // Load the schdule
+  document.getElementById('timeNow').innerHTML = `${hr}:${min}${ampm}`;
+  // Set the stations
   let tripLabels = prefs.tripLabels();
   document.getElementById('origin').innerHTML = tripLabels[0];
   document.getElementById('destin').innerHTML = tripLabels[1];
-  var filler = [
-      [309, '6:04', 'am', '7:09', 'am'],
-      [211, '6:23', 'am', '7:57', 'am'],
-      [217, '6:59', 'am', '8:24', 'am'],
-      [313, '6:49', 'am', '7:52', 'am'],
-      [215, '6:54', 'am', '8:08', 'am'],
-      [319, '7:04', 'am', '8:13', 'am'],
-      [221, '7:23', 'am', '9:00', 'am'],
-      [323, '7:49', 'am', '9:00', 'am'],
-      [329, '8:04', 'am', '9:13', 'am'],
-      [231, '8:23', 'am', '9:52', 'am'],
-      [233, '8:39', 'am','10:09', 'am'],
-      [135, '9:13', 'am','10:52', 'am'],
-      [237, '9:50', 'am','11:19', 'am'],
-      [139,'10:13', 'am','11:48', 'am'],
-      [143,'11:13', 'am','12:48', 'pm'],
-      [147,'12:13', 'pm', '1:48', 'pm'],
-      [151, '1:13', 'pm', '2:48', 'pm'],
-      [155, '2:13', 'pm', '3:52', 'pm'],
-      [257, '2:24', 'pm', '3:57', 'pm'],
-      [159, '3:13', 'pm', '4:53', 'pm'],
-      [261, '3:40', 'pm', '5:02', 'pm'],
-      [263, '4:12', 'pm', '5:38', 'pm'],
-      [365, '4:24', 'pm', '5:33', 'pm'] ];
-  for (var i=0; i < 6; i++) {
-    var data = filler[appState.offset + i]
-    var card = `<div class="train-number">#${data[0]}</div>
-        <div class="train-time">${data[1]}<span class="meridiem">${data[2]}</span></div>
-        <div class="train-time">${data[3]}<span class="meridiem">${data[4]}</span></div>`;
-    var element = document.getElementById(`trip${i}`)
+  // Load the schdule (CaltrainService WEEKDAY: 8)
+  let routes = sched.routes(tripLabels[0], tripLabels[1], WEEKDAY, prefs.swapped);
+  let minsNow = hr * 60 + min;
+  debugger;
+  let minsSet = routes[appState.offset][1];
+  for (let i=0; i < 6; i++) {
+    let data = routes[appState.offset + i];
+    // Should be moved into GoodTimes class
+    let originStr = originMer = destinStr = destinMer = null;
+    let om = data[1] % 60;
+    let dm = data[2] % 60;
+    if (data[1] > 720) {
+      originStr = `${(data[1] - 720) / 60}:${om < 10 ? 0 : ''}${om}`;
+      originMer = 'pm';
+    } else {
+      originStr = `${data[1] / 60}:${om < 10 ? 0 : ''}${om}`;
+      originMer = 'am';
+    }
+    if (data[1] > 720) {
+      destinStr = `${(data[2] - 720) / 60}:${dm < 10 ? 0 : ''}${dm}`;
+      destinMer = 'pm';
+    } else {
+      destinStr = `${data[2] / 60}:${dm < 10 ? 0 : ''}${dm}`;
+      destinMer = 'am';
+    }
+    // Create each trip card.
+    let card = `<div class="train-number">#${data[0]}</div>
+        <div class="train-time">${originStr}<span class="meridiem">${originMer}</span></div>
+        <div class="train-time">${destinStr}<span class="meridiem">${destinMer}</span></div>`;
+    let element = document.getElementById(`trip${i}`)
     element.innerHTML = card;
-    var classes = ['trip-card'];
-    var inThePast = appState.offset + i < 8;
+    let classes = ['trip-card'];
+    let inThePast = appState.offset + i < 8;
     if (inThePast) classes.push('train-departed');
     if (i === 0) {
       classes.push('selection');
       classes.push(inThePast ? 'selection-departed' : 'selection-arriving');
-      fakeCountdown =  countdownFake(fakeTime, data[1] + data[2]);
-      document.getElementById('message').innerHTML = fakeCountdown;
+      document.getElementById('message').innerHTML = countdown(timeNow, selectedTime);
     }
     element.className = classes.join(' ');
   }
 };
 
-// The countdownFake function works with minutes (not seconds)
-var countdownFake = function(clockNowTime, selectedTime) {
-  var clockNowParts = clockNowTime.split(':');
-  var selectedParts = selectedTime.split(':');
-  clockNow = (parseInt(clockNowParts[0]) * 60 + parseInt(clockNowParts[1]));
-  selected = (parseInt(selectedParts[0]) * 60 + parseInt(selectedParts[1]));
-  if (selectedParts[1].endsWith('pm')) { selected += 1440; }
-  var timeDifference = selected - clockNow;
+// Should be moved into GoodTimes class.
+var countdown = function(minsNow, minsSet) {
+  let timeDifference = minsSet - minsNow;
   if (timeDifference < 0) { return '&nbsp;'; }
   if (timeDifference < 60) {
     return `in ${Math.floor(timeDifference % 60)} min 22 sec`;
@@ -94,10 +87,8 @@ var displayMessage = function (message) {
   document.getElementById('message').innerHTML = message;
   if (message === 'up' && appState.offset > 0) {
     appState.offset = appState.offset - 1;
-    loadSchedule();
   } else if (message === 'down' && appState.offset < 17) {
     appState.offset = appState.offset + 1;
-    loadSchedule();
   }
 };
 
@@ -144,19 +135,14 @@ var attachListeners = function () {
       displayMessage('down'); // page down
     } else if (code == 52) { // 4
       prefs.bumpStations(true, false);
-      loadSchedule();
     } else if (code == 54) { // 6
       prefs.bumpStations(true, true);
-      loadSchedule();
     } else if (code == 55) { // 7
       prefs.bumpStations(false, false);
-      loadSchedule();
     } else if (code == 57) { // 9
       prefs.bumpStations(false, true);
-      loadSchedule();
     } else if (code == 48) { // 0
       prefs.swapStations();
-      loadSchedule();
     } else if (code >= 48 && code <= 57) {
       displayMessage('#' + (code - 48));
     } else if (code == 37) {
@@ -169,8 +155,10 @@ var attachListeners = function () {
       displayMessage("down");
     } else {
       displayMessage(code);
+      return;
     }
     openFullScreen();
+    loadSchedule();
   });
 };
 
