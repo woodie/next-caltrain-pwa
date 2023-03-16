7dc55c042804fef8705fbf4aaf2ea31f6e493791
John Woodell <jwoodell@chanzuckerberg.com>
2023-03-16 01:21:20 -0700

    more specs

--- test/CaltrainService.js
+++ test/CaltrainService.js
@@ -2,41 +2,87 @@ import { CaltrainService } from '../src/CaltrainService.js';
 const expect = require('chai').expect;
 
 describe('CaltrainService', () => {
+  let schedule = 'Weekday';
+  let direction = 'North';
+  let departStop = 0;
+  let arriveStop = 6;
+  let routes = [];
+  let skip = [];
+  let minutes = 700;
+  let train = 101;
+
   const cs = new CaltrainService();
+  const departTimes = cs.times(departStop, direction, schedule);
+  const arriveTimes = cs.times(arriveStop, direction, schedule);
 
   describe('.mapStops()', () => {
+    let mapStops = CaltrainService.mapStops(direction);
+
     it('should contain stations', () => {
-      let mapStops = CaltrainService.mapStops('North');
       expect(mapStops.has('Gilroy')).to.be.true;
       expect(mapStops.has('San Francisco')).to.be.true;
     });
   });
 
-  describe('#tripStops()', () => {
+  describe('.tripStops()', () => {
+    let tripStops = CaltrainService.tripStops(train, direction, schedule);
+
     it('should contain stations', () => {
-      let tripStops = CaltrainService.tripStops(101, 'North', 'Weekday');
       expect(tripStops[0]).to.contain('Tamien');
       expect(tripStops[tripStops.length - 1]).to.contain('San Francisco');
     });
   });
 
-  // static direction (departStop, arriveStop) {
-  // static nextIndex (routes, minutes) {
-  // static merge (departTimes, arriveTimes, skip) {
-  // static select (direction, schedule) {
+  describe('.direction()', () => {
+    let direction = CaltrainService.direction(departStop, arriveStop);
 
-  describe('.stopMap()', () => {
+    it('should be North', () => {
+      expect(direction).to.eq('North');
+    });
+  });
+
+  describe('.nextIndex()', () => {
+    let nextIndex = CaltrainService.nextIndex(routes, minutes);
+
+    it('should be zero', () => {
+      expect(nextIndex).to.eq(0);
+    });
+  });
+
+  describe('.merge()', () => {
+    let merge = CaltrainService.merge(departTimes, arriveTimes, skip);
+
+    it('should be empty', () => {
+      expect(merge).to.be.empty;
+    });
+  });
+
+  describe('.select()', () => {
+    let select = CaltrainService.select(direction, schedule);
+
+    it('should have empty members', () => {
+      expect(select['101'][0]).to.be.undefined;
+    });
+  });
+
+  describe('#stopMap()', () => {
     it('should contain stations', () => {
       expect(cs.stopMap().has('Gilroy')).to.be.true;
       expect(cs.stopMap().has('San Francisco')).to.be.true;
     });
   });
 
-  // routes (departStop, arriveStop, schedule) {
-  // times (stop, direction, schedule) {
+  describe('#routes()', () => {
+    it('should be empty', () => {
+      let routes = cs.routes(0, 0, 'Weekday');
+      expect(routes).to.be.empty;
+    });
+  });
 
+  describe('#times()', () => {
+    it('should be empty', () => {
+      let times = cs.times(0, 'North', 'Weekday');
+      expect(times).to.be.empty;
+    });
+  });
 });
-
-
-
-
