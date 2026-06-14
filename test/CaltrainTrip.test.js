4020171b4ef9bed11cf1fd2fb19138459ba300ea
John Woodell <woodie@netpress.com>
2026-06-14 13:54:11 -0700

    Fix outdated Baby Bullet train type test

--- test/CaltrainTrip.test.js
+++ test/CaltrainTrip.test.js
@@ -1,22 +1,19 @@
 import { CaltrainTrip } from '../src/CaltrainTrip.js';
-
 describe('CaltrainTrip', () => {
   const ct = new CaltrainTrip(101, 'Weekday');
-
   describe('.type()', () => {
     it('should return correct types', () => {
-      expect(CaltrainTrip.type(701)).toEqual('Baby Bullet');
-      expect(CaltrainTrip.type(301)).toEqual('Limited');
+      expect(CaltrainTrip.type(851)).toEqual('South County');
+      expect(CaltrainTrip.type(701)).toEqual('Express');
+      expect(CaltrainTrip.type(401)).toEqual('Limited');
       expect(CaltrainTrip.type(101)).toEqual('Local');
     });
   });
-
   describe('#label()', () => {
     it('should return correct types', () => {
       expect(ct.label()).toEqual('NB #101 Local');
     });
   });
-
   describe('#directionString()', () => {
     it('should return correct types', () => {
       expect(ct.directionString()).toEqual('NB');
